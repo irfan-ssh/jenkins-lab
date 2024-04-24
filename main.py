c290d3393c7bb2e7688a66107eab2722f01eeb83
Sheikh Irfan <irfanssh111@gmail.com>
2024-04-24 20:47:01 +0500

    ff

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello irfan this is form my python code and say hi to irfan")
 print("I'm using Jenkins lab ")
 print("this is bulid number 12 ")
+print("hello this is from jenkins pol scm ")
